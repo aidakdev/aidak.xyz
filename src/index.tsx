e879e5eacfb87937e238cf5a665a2e60af0ebb97
aidakx <aidaksupport@protonmail.com>
2022-06-09 19:53:00 +0200

    route things

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ReactRouterDOM from 'react-router-dom'
 
-import { BrowserRouter } from 'react-router-dom'
+import Home from './pages/home'
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 )
 
 root.render(
-    <BrowserRouter>
-    </BrowserRouter>
+    <ReactRouterDOM.BrowserRouter>
+        <div className='app'>
+            <ReactRouterDOM.Routes>
+                <ReactRouterDOM.Route path='/' element={Home} />
+            </ReactRouterDOM.Routes>
+        </div>
+    </ReactRouterDOM.BrowserRouter>
 )
---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// import '../styles/home.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default (
+    <div className='content'>
+        <h1>ola</h1>
+    </div>
+)
