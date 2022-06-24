cafe0ce64b77e13672b2343e9f655d82f835d487
aidakx <aidaksupport@protonmail.com>
2022-06-24 23:59:56 +0200

    discord card base

---
+++ src/components/discord.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/discord.css'
+
+const DiscordCard = () => {
+    return (
+        <div className='discord-card'>
+            <h1>Aidak</h1>
+        </div>
+    )
+}
+
+export default DiscordCard
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 import '../styles/home.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
+import DiscordCard from '../components/discord'
+
 export default (
     <div className='content'>
-        <h1>ola</h1>
+        <DiscordCard />
     </div>
 )
